3e578607dab4f5b8d73dc8c4a287a69ccf09507d
Francesco Rodriguez <lrodriguezsanc@gmail.com>
2013-06-26 13:02:21 -0500

    Add basic search form

--- app/controllers/gems_controller.rb
+++ app/controllers/gems_controller.rb
@@ -1,7 +1,11 @@
 class GemsController < ApplicationController
-
   def index
-    @gems = Rubygem.alphabetically
-  end
+    scope = Rubygem.alphabetically
 
+    @gems = if params[:query].present?
+      scope.search_by_name params[:query]
+    else
+      scope
+    end
+  end
 end
--- app/models/rubygem.rb
+++ app/models/rubygem.rb
@@ -1,8 +1,9 @@
 class Rubygem < ActiveRecord::Base
-  STATUSES = ['compatible', 'not compatible', 'unknown']
+  STATUSES = ["compatible", "not compatible", "unknown"]
 
   validates :name,   presence: true, uniqueness: true
   validates :status, presence: true, inclusion: STATUSES
 
   scope :alphabetically, -> { order "name" }
+  scope :search_by_name, ->(name) { where "name ILIKE '%#{name}%'" }
 end

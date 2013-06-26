0ff5373fed8e4bd89e0e334d4016b542f8d308d0
Florent Guilleux <florent2@gmail.com>
2013-06-26 14:23:50 -0500

    Allow registering a gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails4ready::Application.routes.draw do
-  resources :gems, except: :destroy
-  root to: "gems#index"
+  resources :rubygems, except: :destroy, path: "gems"
+  root to: "rubygems#index"
 end

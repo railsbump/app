d683ff6aee272a2f3c25ce5739780f72a82406f8
Francesco Rodriguez <lrodriguezsanc@gmail.com>
2013-06-28 18:44:46 -0500

    Refactor GemParser class

--- lib/gemfile_parser.rb
+++ lib/gemfile_parser.rb
@@ -1,6 +1,8 @@
 class GemfileParser
   attr_reader :gemfile
 
+  EXCLUDED = ["rails"]
+
   def self.gems_status gemfile
     new(gemfile).gems_status
   end
@@ -10,11 +12,16 @@ class GemfileParser
   end
 
   def gems_status
-    gemfile
-      .scan(/gem\s+['"](\w+)['"]/)
-      .flatten
-      .reject { |gem| gem == "rails" }
-      .map { |gem| Rubygem.by_name(gem).first }
-      .compact
+    Rubygem.where(name: gems_without_excluded).to_a
+  end
+
+  private
+
+  def gems_without_excluded
+    gems.reject { |gem| EXCLUDED.include? gem }
+  end
+
+  def gems
+    gemfile.scan(/gem\s+['"](\w+)['"]/).flatten
   end
 end

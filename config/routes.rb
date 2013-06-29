173edd44981f32d3a054c5ba7fe982b7dfd9ec7e
Francesco Rodriguez <lrodriguezsanc@gmail.com>
2013-06-29 12:31:34 -0500

    Add redirect and rest in peace

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Ready4rails4::Application.routes.draw do
   resources :rubygems, except: :destroy, path: "gems"
   resource  :gemfile_check, only: [:new, :create]
+  get "/gemfile/new", to: redirect("/gemfile_check/new")
 
   root to: "rubygems#index"
 end

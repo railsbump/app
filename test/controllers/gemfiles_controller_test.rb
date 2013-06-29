7a45f21270210a7015d351ac74f568890a3af45e
Francesco Rodriguez <lrodriguezsanc@gmail.com>
2013-06-28 19:06:08 -0500

    Rubygem#where must be called

--- test/controllers/gemfiles_controller_test.rb
+++ test/controllers/gemfiles_controller_test.rb
@@ -3,8 +3,8 @@ require "test_helper"
 class GemfilesControllerTest < ActionController::TestCase
   test "#create returns existing gems from Gemfile" do
     gemfile, parsed, gems = "gemfile", stub, stub
-    GemfileParser.stubs(:gems).with(gemfile).returns parsed
-    Rubygem.stubs(:where).with(name: parsed).returns gems
+    GemfileParser.expects(:gems).with(gemfile).returns parsed
+    Rubygem.expects(:where).with(name: parsed).returns gems
 
     post :create, gemfile: gemfile
 

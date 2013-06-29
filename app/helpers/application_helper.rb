df8d1d441e65fba583216f0251acabdbe66a298a
Florent Guilleux <florent2@gmail.com>
2013-06-29 11:57:27 -0500

    Display status in color in the index view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,11 @@
 module ApplicationHelper
-  Markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true)
+  def display_status gem
+    content_tag :span, gem.status, class: gem.status.gsub(" ", "-")
+  end
 
+  Markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true)
   def markdown content
     Markdown.render(content).html_safe if content.present?
   end
+
 end

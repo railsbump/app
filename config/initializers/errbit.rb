ae627df8f5f22717e12c715047080a91076eb915
Florent Guilleux <florent2@gmail.com>
2013-06-29 16:59:01 -0500

    For now ignore no exceptions for Errbit

--- config/initializers/errbit.rb
+++ config/initializers/errbit.rb
@@ -1,6 +1,7 @@
 Airbrake.configure do |config|
-  config.api_key = ENV['ERRBIT_API_KEY']
-  config.host    = 'ready4rails4-errbit.herokuapp.com'
-  config.port    = 80
-  config.secure  = config.port == 443
+  config.api_key     = ENV['ERRBIT_API_KEY']
+  config.host        = 'ready4rails4-errbit.herokuapp.com'
+  config.port        = 80
+  config.secure      = config.port == 443
+  config.ignore_only = []
 end

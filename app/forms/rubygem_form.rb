7fe941330735749327637084f87c2d5c91310849
Francesco Rodriguez <lrodriguezsanc@gmail.com>
2013-06-29 13:06:10 -0500

    Fix case sensitive bug

--- app/forms/rubygem_form.rb
+++ app/forms/rubygem_form.rb
@@ -12,7 +12,7 @@ class RubygemForm < Reform::Form
 
   model :rubygem
 
-  validates :name,   presence: true, uniqueness: true
+  validates :name,   presence: true, uniqueness: { case_sensitive: false }
   validates :status, presence: true, inclusion: STATUSES
   validates :miel,   format: { without: /.+/ }
 

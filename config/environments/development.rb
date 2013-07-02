df56b64f2dc0a6833f4a4c28c7df5b5a68ac5cd5
Francesco Rodriguez <lrodriguezsanc@gmail.com>
2013-07-02 18:37:22 -0500

    Set default host as localhost in development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -19,6 +19,8 @@ Ready4rails4::Application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
+  config.action_mailer.default_url_options = { host: 'localhost' }
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 

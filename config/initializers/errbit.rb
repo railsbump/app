2fb35da4837eaae5c4dffc330f5f8e970a4e4504
Francesco Rodriguez <frodsan@me.com>
2013-11-17 16:31:21 -0500

    Remove Rails legacy files and directories.

--- config/initializers/errbit.rb
+++
@@ -1,7 +0,0 @@
-Airbrake.configure do |config|
-  config.api_key     = ENV['ERRBIT_API_KEY']
-  config.host        = 'ready4rails4-errbit.herokuapp.com'
-  config.port        = 80
-  config.secure      = config.port == 443
-  config.ignore_only = []
-end

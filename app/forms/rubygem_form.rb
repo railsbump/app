850545dff254b95fab5b75ba4ed7870ced620d83
Florent Guilleux <florent2@gmail.com>
2014-02-11 16:05:17 -0500

    Check if gem name has valid format

--- app/forms/rubygem_form.rb
+++ app/forms/rubygem_form.rb
@@ -1,8 +1,19 @@
 require 'reform/rails'
 
 class ExistsInRubygemsDotOrgValidator < ActiveModel::EachValidator
-  def validate_each(record, attribute, value)
+  def validate_each record, attribute, value
     record.errors[attribute] << "is not the name of a gem registered in http://rubygems.org" if Net::HTTP.get("rubygems.org", "/api/v1/gems/#{value}.json") == "This rubygem could not be found."
+  end
+end
+
+class HasRubygemNameFormatValidator < ActiveModel::EachValidator
+  SPECIAL_CHARACTERS = ".-_"
+  ALLOWED_CHARACTERS = "[A-Za-z0-9#{Regexp.escape(SPECIAL_CHARACTERS)}]+"
+  NAME_PATTERN       = /\A#{ALLOWED_CHARACTERS}\Z/
+
+  def validate_each record, attribute, value
+    record.errors[attribute] << "must include at least one letter" if value !~ /[a-zA-Z]+/
+    record.errors[attribute] << "can only include letters, numbers, dashes, and underscores" if value !~ NAME_PATTERN
   end
 end
 
@@ -16,7 +27,7 @@ class RubygemForm < Reform::Form
 
   model :rubygem
 
-  validates :name,   presence: true, uniqueness: { case_sensitive: false }, exists_in_rubygems_dot_org: true
+  validates :name,   presence: true, uniqueness: { case_sensitive: false }, has_rubygem_name_format: true, exists_in_rubygems_dot_org: true
   validates :status, presence: true, inclusion: Rubygem::STATUSES
   validates :notes,  presence: true
   validates :miel,   format: { without: /.+/ }

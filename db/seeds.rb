4f32c6f7701010ef7b24970098190d2aa52c2b63
Florent Guilleux <contact@florentguilleux.fr>
2015-11-18 10:53:25 +0100

    Fix seed file #15
    `rake db:seed` was broken because it was using old attribute names
    (`notes` and `status` instead of `notes_rails4` and `status_rails4`)
    and gem names were uppercased, while on rubygems.org they are not.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,23 @@
 Rubygem.create!(
-  name:   "Devise",
-  status: "ready",
-  notes:  "From 3.0rc gem version"
+  name:   "devise",
+  status_rails4: "ready",
+  status_rails5: "unknown",
+  notes_rails4:  "From 3.0rc gem version",
+  notes_rails5:  ""
 )
 
 Rubygem.create!(
-  name:   "CanCan",
-  status: "unknown",
-  notes:  "Maybe ready in the 2.0 branch"
+  name:   "cancan",
+  status_rails4: "unknown",
+  status_rails5: "unknown",
+  notes_rails4:  "Maybe ready in the 2.0 branch",
+  notes_rails5:  ""
 )
 
 Rubygem.create!(
   name:   "roadie",
-  status: "not ready"
+  status_rails4: "not ready",
+  status_rails5: "not ready",
+  notes_rails4:  "see issue #xxx",
+  notes_rails5:  "see issue #yyy",
 )

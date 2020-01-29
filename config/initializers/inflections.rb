0b0b5292c2c64961f316835e30534d24ae83bbf1
Manuel Meurer <manuel@krautcomputing.com>
2020-01-30 00:54:45 +0100

    add API acronym

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -9,6 +9,6 @@
 # end
 
 # These inflection rules are supported but not enabled by default:
-# ActiveSupport::Inflector.inflections(:en) do |inflect|
-#   inflect.acronym 'RESTful'
-# end
+ActiveSupport::Inflector.inflections :en do |inflect|
+  inflect.acronym 'API'
+end

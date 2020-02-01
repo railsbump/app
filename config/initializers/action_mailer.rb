0a9af45e3aff4df16a864238a9334b38ab872d76
Manuel Meurer <manuel@krautcomputing.com>
2020-02-01 22:15:40 +0100

    fix env var name

--- config/initializers/action_mailer.rb
+++ config/initializers/action_mailer.rb
@@ -1,7 +1,7 @@
 ActionMailer::Base.smtp_settings = {
   port:           ENV['MAIL_PORT'],
   address:        ENV['MAIL_ADDRESS'],
-  user_name:      ENV['MAIL_USER_NAME'],
+  user_name:      ENV['MAIL_USERNAME'],
   password:       ENV['MAIL_PASSWORD'],
   domain:         ENV['MAIL_DOMAIN'],
   authentication: ENV['MAIL_AUTHENTICATION']

ac7112b6445fb8043aff436927c5b366972e5d46
Manuel Meurer <manuel@krautcomputing.com>
2020-02-01 22:15:35 +0100

    set default_url_options

--- config/application.rb
+++ config/application.rb
@@ -8,5 +8,11 @@ module RailsBump
   class Application < Rails::Application
     config.load_defaults 6.0
     config.current_version = `git rev-parse --short HEAD 2> /dev/null`.chomp
+
+    Rails.application.routes.default_url_options =
+      config.action_mailer.default_url_options = {
+        host:     ENV.fetch('HOST'),
+        protocol: 'https'
+      }
   end
 end

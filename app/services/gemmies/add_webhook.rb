cd92b07bc4ccf3262c3999157b6dfe36e0c101e6
Manuel Meurer <manuel@krautcomputing.com>
2020-02-08 23:40:55 +0100

    check if rubygems api key is set instead of rails env

--- app/services/gemmies/add_webhook.rb
+++ app/services/gemmies/add_webhook.rb
@@ -3,7 +3,7 @@ require 'gems'
 module Gemmies
   class AddWebhook < Services::Base
     def call(gemmy)
-      if Rails.env.production?
+      if Gems.key.present?
         Gems.add_web_hook gemmy.name, api_releases_url
       end
     end

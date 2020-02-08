ceb703761317ce2237765b2b40c81a3d13aed989
Manuel Meurer <manuel@krautcomputing.com>
2020-02-08 23:43:25 +0100

    fix param

--- app/controllers/api/travis_notifications_controller.rb
+++ app/controllers/api/travis_notifications_controller.rb
@@ -1,7 +1,7 @@
 module API
   class TravisNotificationsController < BaseController
     def create
-      travis_notification = TravisNotification.create!(payload: JSON.load(params[:payload]))
+      travis_notification = TravisNotification.create!(data: JSON.load(params[:payload]))
       TravisNotifications::Process.call_async travis_notification
 
       head :ok

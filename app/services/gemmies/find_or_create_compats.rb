b8dd792566d7144cde282edb0798cdbfbc868547
Manuel Meurer <manuel@krautcomputing.com>
2020-02-15 21:30:28 +0100

    create compats grouped by rails release, not gem dependencies

--- app/services/gemmies/find_or_create_compats.rb
+++ app/services/gemmies/find_or_create_compats.rb
@@ -3,8 +3,8 @@ require 'gems'
 module Gemmies
   class FindOrCreateCompats < Services::Base
     def call(gemmy)
-      gemmy.dependencies.flat_map do |dependencies|
-        RailsRelease.latest_major.map do |rails_release|
+      RailsRelease.latest_major.map do |rails_release|
+        gemmy.dependencies.flat_map do |dependencies|
           rails_release.compats.where(dependencies: dependencies).first_or_create!
         end
       end

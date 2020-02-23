cb9d4e7870f5eeb1bd2bb99bef1b33b682d6c228
Manuel Meurer <manuel@krautcomputing.com>
2020-02-23 13:55:20 +0100

    add rollbar sidekiq jobs to default queue

--- config/initializers/rollbar.rb
+++ config/initializers/rollbar.rb
@@ -42,9 +42,9 @@ Rollbar.configure do |config|
   # config.use_sucker_punch
 
   # Enable delayed reporting (using Sidekiq)
-  config.use_sidekiq
+  # config.use_sidekiq
   # You can supply custom Sidekiq options:
-  # config.use_sidekiq 'queue' => 'default'
+  config.use_sidekiq 'queue' => 'default'
 
   # If your application runs behind a proxy server, you can set proxy parameters here.
   # If https_proxy is set in your environment, that will be used. Settings here have precedence.

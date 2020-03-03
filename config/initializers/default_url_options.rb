e853b740680ab3f8cbbbd21cdbdeec6a1a2ce497
Manuel Meurer <manuel@krautcomputing.com>
2020-03-03 10:50:27 +0100

    fix default_url_options initializer

--- config/initializers/default_url_options.rb
+++ config/initializers/default_url_options.rb
@@ -2,7 +2,7 @@
 # since the environment variable HOST is used,
 # which is loaded in the _envkey.rb initializer.
 Rails.application.routes.default_url_options =
-  config.action_mailer.default_url_options = {
+  Rails.application.config.action_mailer.default_url_options = {
     host:     ENV.fetch('HOST') { 'localhost:3000' },
     protocol: 'https'
   }

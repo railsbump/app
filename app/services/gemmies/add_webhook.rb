d24cf766137d271448563a47f5840a5a30218616
Manuel Meurer <manuel@krautcomputing.com>
2020-03-11 12:55:04 +0100

    check if webhook already exists before adding it

--- app/services/gemmies/add_webhook.rb
+++ app/services/gemmies/add_webhook.rb
@@ -3,7 +3,7 @@ require 'gems'
 module Gemmies
   class AddWebhook < Services::Base
     def call(gemmy)
-      if Gems.key.present?
+      if Gems.key.present? && Gems.web_hooks.keys.exclude?(gemmy.name)
         Gems.add_web_hook gemmy.name, api_releases_url
       end
     end

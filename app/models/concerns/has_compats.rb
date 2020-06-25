549e06e647c08c8d8b3536cad0051862bcfed5b3
Manuel Meurer <manuel@krautcomputing.com>
2020-06-26 00:55:50 +0200

    fix typo

--- app/models/concerns/has_compats.rb
+++ app/models/concerns/has_compats.rb
@@ -1,6 +1,6 @@
 module HasCompats
   def compats
-    dependencies = is_a?(Gemmy) ? dependencies : gemmies.map(&:dependencies)
+    dependencies = is_a?(Gemmy) ? self.dependencies : gemmies.map(&:dependencies)
     Compat.where(dependencies: dependencies, rails_release: RailsRelease.latest_major)
   end
 end

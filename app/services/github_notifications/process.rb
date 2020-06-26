27fcd44b3187948da157bc40b174313725c35b4e
Manuel Meurer <manuel@krautcomputing.com>
2020-06-26 09:11:13 +0200

    fix processing github notifications

--- app/services/github_notifications/process.rb
+++ app/services/github_notifications/process.rb
@@ -7,17 +7,17 @@ module GithubNotifications
 
       action, conclusion = github_notification.data.fetch_values('action', 'conclusion')
 
-      next unless action == 'completed'
+      if action == 'completed'
+        unless conclusion.in?(%w(success failure))
+          raise Error, "Expected conclusion to be 'success' or 'error', but got: #{conclusion}" rescue Rollbar.error $!, github_notification_id: github_notification.id
+        end
 
-      unless conclusion.in?(%w(success failure))
-        raise Error, "Expected conclusion to be 'success' or 'error', but got: #{conclusion}" rescue Rollbar.error $!, github_notification_id: github_notification.id
+        compatible = conclusion == 'success'
+        compat     = Compat.find(branch)
+
+        compat.update! compatible: compatible
+        github_notification.update! compat: compat
       end
-
-      compatible = conclusion == 'success'
-      compat     = Compat.find(branch)
-
-      compat.update! compatible: compatible
-      github_notification.update! compat: compat
 
       github_notification.processed!
     end

7fe80c0bfb0f04e50b562dbbdba7e6087f372263
Manuel Meurer <manuel@krautcomputing.com>
2020-08-03 14:54:14 +0200

    improve timestamps

--- app/models/concerns/has_timestamps.rb
+++ app/models/concerns/has_timestamps.rb
@@ -3,8 +3,8 @@ module HasTimestamps
     Module.new do
       extend ActiveSupport::Concern
 
-      attrs_and_verbs = attrs.each_with_object({}) do |attr, hash|
-        hash[attr] = attr.to_s.sub(/_(at|on)\z/, '')
+      attrs_and_verbs = attrs.index_with do |attr|
+        attr.to_s.sub(/_(at|on)\z/, '')
       end
 
       included do
@@ -14,17 +14,19 @@ module HasTimestamps
           scope "un#{verb}", -> { where(attr_with_table_name => nil) }
 
           scope "#{verb}_between", ->(start_time, end_time) {
-            unless start_time && end_time
-              raise "Start time and end time must be supplied when calling #{self}.#{verb}_between."
-            end
             if start_time >= end_time
-              raise "Start time must be before end time when calling #{self}.#{verb}_between."
+              raise 'Start time must be before end time.'
             end
             where(attr_with_table_name => start_time..end_time)
           }
 
-          { before: '<', after: '>' }.each do |before_or_after, operator|
-            scope "#{verb}_#{before_or_after}", ->(time) { where("#{attr_with_table_name} #{operator} ?", time) }
+          { before: %w(< >), after: %w(> <) }.each do |before_or_after, (operator, unoperator)|
+            scope "#{verb}_#{before_or_after}", ->(time = Time.current) {
+              where("#{attr_with_table_name} #{operator} ?", time)
+            }
+            scope "un#{verb}_#{before_or_after}", ->(time = Time.current) {
+              where("(#{attr_with_table_name} IS NULL) OR (#{attr_with_table_name} #{unoperator} ?)", time)
+            }
           end
         end
       end
@@ -36,7 +38,7 @@ module HasTimestamps
             if values.any?(&:blank?)
               value = nil
             else
-              values.map! { _1.is_a?(String) ? Time.zone.parse(_1) : _1.to_time }
+              values.map! { |v| v.is_a?(String) ? Time.zone.parse(v) : v.to_time }
               date, time = values
               value = date.change(hour: time.hour, min: time.min, sec: time.sec)
             end
@@ -54,6 +56,19 @@ module HasTimestamps
           !public_send("#{verb}?")
         end
         alias_method "un#{verb}", "un#{verb}?"
+
+        define_method "#{verb}_between?" do |start_time, end_time|
+          if start_time >= end_time
+            raise 'Start time must be before end time.'
+          end
+          public_send("#{verb}?") && (start_time..end_time).cover?(public_send(attr))
+        end
+
+        %i(before after).each do |before_or_after|
+          define_method "#{verb}_#{before_or_after}?" do |time = Time.current|
+            public_send("#{verb}?") && (before_or_after == :before ? public_send(attr) < time : public_send(attr) > time)
+          end
+        end
 
         define_method "#{verb}!" do |time = Time.current, overwrite: true|
           if overwrite || public_send("un#{verb}?")

3a957624d74afbed4cf858a3be39198f3d1c3feb
Manuel Meurer <manuel@krautcomputing.com>
2020-08-07 23:58:31 +0200

    `Gemmies::FindOrCreateCompats` should not run mutiple times simultaneously for the same gem

--- app/services/gemmies/find_or_create_compats.rb
+++ app/services/gemmies/find_or_create_compats.rb
@@ -3,6 +3,8 @@ require 'gems'
 module Gemmies
   class FindOrCreateCompats < Services::Base
     def call(gemmy)
+      check_uniqueness gemmy.id, on_error: :return
+
       RailsRelease.latest_major.map do |rails_release|
         gemmy.dependencies.flat_map do |dependencies|
           rails_release.compats.where(dependencies: dependencies).first_or_create!

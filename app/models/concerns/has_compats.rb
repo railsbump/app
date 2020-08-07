19dc2fda58a3895f98cea52a7034025aa1a5173b
Manuel Meurer <manuel@krautcomputing.com>
2020-08-07 23:43:07 +0200

    fix finding compats for lockfile

--- app/models/concerns/has_compats.rb
+++ app/models/concerns/has_compats.rb
@@ -1,6 +1,6 @@
 module HasCompats
   def compats
-    dependencies = is_a?(Gemmy) ? self.dependencies : gemmies.map(&:dependencies)
+    dependencies = is_a?(Gemmy) ? self.dependencies : gemmies.flat_map(&:dependencies).uniq
     Compat.where(dependencies: dependencies, rails_release: RailsRelease.latest_major)
   end
 end

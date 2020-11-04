958fea8d6367b5171574100ff1ee32fcd8d5f239
Manuel Meurer <manuel@krautcomputing.com>
2020-11-04 20:29:45 +0100

    include only the fontawesome icons that are actually used, fixes #47

--- app/javascript/application/font-awesome.js
+++ app/javascript/application/font-awesome.js
@@ -1,16 +1,24 @@
 import { config, library, dom } from '@fortawesome/fontawesome-svg-core'
 
-import { fas as fs } from '@fortawesome/free-solid-svg-icons'
-import { far as fr } from '@fortawesome/free-regular-svg-icons'
-import { fab as fb } from '@fortawesome/free-brands-svg-icons'
-import { fas as ps } from '@fortawesome/pro-solid-svg-icons'
-import { far as pr } from '@fortawesome/pro-regular-svg-icons'
-import { fal as pl } from '@fortawesome/pro-light-svg-icons'
-import { fad as pd } from '@fortawesome/pro-duotone-svg-icons'
+import {
+  faThumbsUp          as farThumbsUp,
+  faThumbsDown        as farThumbsDown
+} from '@fortawesome/pro-regular-svg-icons'
+
+import {
+  faSpinner           as fasSpinner
+} from '@fortawesome/pro-solid-svg-icons'
 
 // https://fontawesome.com/how-to-use/on-the-web/using-with/turbolinks
 config.mutateApproach = 'sync'
 
-library.add(fs, fr, fb, ps, pr, pl, pd)
+library.add(
+  // Regular
+  farThumbsUp,
+  farThumbsDown,
+
+  // Solid
+  fasSpinner
+)
 
 dom.watch()

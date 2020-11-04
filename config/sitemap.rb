1eb82a124db0f8a6e2c5c0f41739af60ddba1434
Manuel Meurer <manuel@krautcomputing.com>
2020-11-04 22:39:11 +0100

    compress sitemaps

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -4,7 +4,6 @@ require 'aws-sdk-s3'
 SitemapGenerator::Sitemap.default_host  = 'https://railsbump.org'
 SitemapGenerator::Sitemap.sitemaps_host = 'https://railsbump.s3.eu-central-1.amazonaws.com'
 SitemapGenerator::Sitemap.adapter       = SitemapGenerator::AwsSdkAdapter.new('railsbump')
-SitemapGenerator::Sitemap.compress      = false
 SitemapGenerator::Sitemap.create do
   add new_gemmy_path
   add new_lockfile_path

a863acf440b8fa525c2265244b1d66c95244419d
Manuel Meurer <manuel@krautcomputing.com>
2021-01-20 07:38:28 +0100

    update migration

--- db/migrate/20191222065024_create_gemmies.rb
+++ db/migrate/20191222065024_create_gemmies.rb
@@ -3,6 +3,7 @@ class CreateGemmies < ActiveRecord::Migration[6.0]
     create_table :gemmies do |t|
       t.string :name, index: { unique: true }
       t.jsonb :dependencies_and_versions, default: {}
+      t.text :compat_ids, array: true, default: [], null: false
       t.timestamps
     end
   end

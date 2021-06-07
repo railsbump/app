7ae484cd11f5c7b32615adba5bdb8fb86902681e
Manuel Meurer <manuel@krautcomputing.com>
2021-06-07 11:24:12 +0200

    fix processing new rails releases

--- app/services/rails_releases/process.rb
+++ app/services/rails_releases/process.rb
@@ -1,13 +1,8 @@
 module RailsReleases
   class Process < Services::Base
     def call(rails_release)
-      # We only need to process each unique set of dependencies once.
-      # Is there a way to get to them without resorting to `pluck`, which would load them all into memory?
-      processed_dependencies = []
-      Compat.where.not(rails_release: rails_release).find_each do |compat|
-        next if processed_dependencies.include?(compat.dependencies)
-        rails_release.compats.where(dependencies: compat.dependencies).first_or_create!
-        processed_dependencies << compat.dependencies
+      Gemmy.find_each do |gemmy|
+        Gemmies::UpdateCompats.call_async(gemmy)
       end
 
       Compats::CheckAllUnchecked.call_async

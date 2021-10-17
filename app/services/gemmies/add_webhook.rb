9a67a85039e9ec7035cf7f17cb54c5d180698799
Manuel Meurer <manuel@krautcomputing.com>
2021-10-17 20:16:02 +0200

    try to add rubygems webhook and ignore error if it exists, instead of checking existence first

--- app/services/gemmies/add_webhook.rb
+++ app/services/gemmies/add_webhook.rb
@@ -2,9 +2,17 @@ require 'gems'
 
 module Gemmies
   class AddWebhook < Services::Base
+    IGNORED_ERROR_MESSAGE = /has already been registered/
+
     def call(gemmy)
-      if Gems.key.present? && Gems.web_hooks.keys.exclude?(gemmy.name)
+      return unless Gems.key.present?
+
+      begin
         Gems.add_web_hook gemmy.name, api_releases_url
+      rescue Gems::GemError => error
+        unless IGNORED_ERROR_MESSAGE.match?(error.message)
+          raise error
+        end
       end
     end
   end

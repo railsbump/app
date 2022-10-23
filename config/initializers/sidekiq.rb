8a6c88d0c2987109812bef6bf3a470ff15b7676c
Manuel Meurer <manuel@meurer.io>
2022-10-23 12:47:21 +0200

    add sidekiq initializer

---
+++ config/initializers/sidekiq.rb
@@ -0,0 +1,16 @@
+redis_options = {
+  url:       ENV.fetch("REDIS_URL"),
+  namespace: "sidekiq"
+}
+
+Sidekiq.configure_server do |config|
+  config.redis = redis_options
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = redis_options.merge(size: 1)
+end
+
+Sidekiq.default_job_options = {
+  "retry" => false
+}

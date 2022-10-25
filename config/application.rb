63387f4b686ccb0f777d10ff8f6ecdfe4b83da2f
Manuel Meurer <manuel@meurer.io>
2022-10-25 21:27:45 +0200

    config fixes

--- config/application.rb
+++ config/application.rb
@@ -6,14 +6,14 @@ require "rails/all"
 
 Bundler.require(*Rails.groups)
 
-module Railsbump
+module RailsBump
   class Application < Rails::Application
     config.load_defaults 7.0
     config.revision = `git rev-parse --short HEAD 2> /dev/null`.chomp
     Rails.application.routes.default_url_options =
       config.action_mailer.default_url_options = {
         host:     ENV.fetch("HOST"),
-        protocol: ENV.fetch("PROTOCOL")
+        protocol: "https"
       }
   end
 end

c6cb36ec0d7c647ff334f2ba2d034b218871af7d
Manuel Meurer <manuel@meurer.io>
2022-10-27 21:50:50 +0200

    add rack cors and deflater

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,15 @@ module RailsBump
     config.load_defaults 7.0
     config.revision = `git rev-parse --short HEAD 2> /dev/null`.chomp
     config.action_mailer.delivery_method = :postmark
+
+    config.middleware.insert 0, Rack::Deflater
+    config.middleware.insert 0, Rack::Cors do
+      allow do
+        origins "*"
+        resource "*", headers: :any, methods: %i(get post options)
+      end
+    end
+
     Rails.application.routes.default_url_options =
       config.action_mailer.default_url_options = {
         host:     ENV.fetch("HOST"),

2b8a935f33ad3444fcbe71621560b31881062774
Manuel Meurer <manuel@meurer.io>
2022-10-29 12:39:37 +0200

    add basic auth to sidekiq, fixes #46

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,16 @@
 require "sidekiq/web"
 require "sidekiq-scheduler/web"
 
+if Rails.env.production?
+  Sidekiq::Web.use Rack::Auth::Basic do |username, password|
+    {
+      username => "SIDEKIQ_USERNAME",
+      password => "SIDEKIQ_PASSWORD"
+    }.map { ActiveSupport::SecurityUtils.secure_compare _1, ENV.fetch(_2) }
+     .all?
+  end
+end
+
 Rails.application.routes.draw do
   mount Sidekiq::Web => "sidekiq"
 

a599f3238394b7650e6dd7e62e37556f14aefff8
Manuel Meurer <manuel@meurer.io>
2022-11-14 15:51:23 +0100

    allow all subdomains as hosts

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -3,8 +3,6 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
-  config.hosts << "railsbump.test"
-
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
@@ -69,4 +67,6 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  config.hosts << /([a-z]+\.)?#{ENV.fetch "HOST"}/
 end

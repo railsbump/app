617a510884096acba81d75d4510ea030e2c8201e
Manuel Meurer <manuel@meurer.io>
2022-11-20 22:59:24 +0100

    remove code that is now part of baseline

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -2,17 +2,4 @@
 
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
-
-  def self.inherited(subclass)
-    super
-
-    if subclass.table_exists?
-      timestamps = %w(created_at updated_at).select do |timestamp|
-        subclass.column_names.include?(timestamp)
-      end
-      if timestamps.any?
-        subclass.include HasTimestamps[*timestamps]
-      end
-    end
-  end
 end

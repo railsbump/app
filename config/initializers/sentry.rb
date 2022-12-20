b62ef94cedea34b67aa3ededbda2fde4a242ae28
Manuel Meurer <manuel@meurer.io>
2022-12-20 10:43:43 +0100

    remove sentry sampling

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -5,13 +5,4 @@ Sentry.init do |config|
   config.capture_exception_frame_locals = true
   config.release                        = Rails.configuration.revision
   config.send_default_pii               = true
-  config.traces_sampler = ->(sampling_context) {
-    # If this is the continuation of a trace,
-    # just use that decision (rate controlled by the caller).
-    if parent_sampled = sampling_context[:parent_sampled]
-      next parent_sampled
-    end
-
-    ENV.fetch("SENTRY_TRACES_SAMPLE_RATE").to_f
-  }
 end

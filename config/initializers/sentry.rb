65386074aaf7a08c68a857633fc2a9ff4e7042e3
Manuel Meurer <manuel@meurer.io>
2023-05-15 20:22:17 +0200

    update sentry config

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 Sentry.init do |config|
-  config.breadcrumbs_logger             = %i(active_support_logger http_logger)
-  config.capture_exception_frame_locals = true
-  config.release                        = Rails.configuration.revision
-  config.send_default_pii               = true
+  config.breadcrumbs_logger      = %i(active_support_logger http_logger)
+  config.include_local_variables = true
+  config.release                 = Rails.configuration.revision
+  config.send_default_pii        = true
 end

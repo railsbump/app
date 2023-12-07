407a5bb276cfd4c330376c908e559aa9a1ed66c0
Manuel Meurer <manuel@meurer.io>
2023-12-07 14:59:26 +0100

    fix migration

--- db/migrate/20191222065024_create_gemmies.rb
+++ db/migrate/20191222065024_create_gemmies.rb
@@ -3,7 +3,7 @@ class CreateGemmies < ActiveRecord::Migration[6.0]
     create_table :gemmies do |t|
       t.string :name, index: { unique: true }
       t.json :dependencies_and_versions, default: {}
-      t.text :compat_ids, array: true, default: [], null: false
+      t.json :compat_ids, default: [], null: false
       t.check_constraint "JSON_TYPE(compat_ids) = 'array'", name: "gemmy_compat_ids_is_array"
       t.timestamps
     end

692591765b6b7c739de12d39d541bdc30d260669
Manuel Meurer <manuel@meurer.io>
2024-01-11 11:02:25 +0100

    formatting, use 3 puma threads by default

--- config/puma.rb
+++ config/puma.rb
@@ -1,7 +1,7 @@
 require "sidekiq"
 
-max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
-min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
+max_threads_count = ENV.fetch("RAILS_MAX_THREADS", 3)
+min_threads_count = ENV.fetch("RAILS_MIN_THREADS", max_threads_count)
 threads min_threads_count, max_threads_count
 
 if ENV["RAILS_ENV"] == "production"
@@ -16,11 +16,9 @@ if ENV.fetch("RAILS_ENV", "development") == "development"
   worker_timeout 3600
 end
 
-port ENV.fetch("PORT") { 3000 }
-
-environment ENV.fetch("RAILS_ENV") { "development" }
-
-pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
+port        ENV.fetch("PORT",      3000)
+environment ENV.fetch("RAILS_ENV", "development")
+pidfile     ENV.fetch("PIDFILE",   "tmp/pids/server.pid")
 
 plugin :tmp_restart
 

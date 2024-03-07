4cbe658481c9811d8a45e9931a3bd3dfe3920387
Manuel Meurer <manuel@meurer.io>
2024-03-07 10:04:03 +0100

    replace Gems.dependencies, it doesnt work anymore

--- app/services/gemmies/update_dependencies_and_versions.rb
+++ app/services/gemmies/update_dependencies_and_versions.rb
@@ -3,12 +3,31 @@ require "gems"
 module Gemmies
   class UpdateDependenciesAndVersions < Baseline::Service
     def call(gemmy)
-      dependencies_and_versions = Gems.dependencies(gemmy.name).each_with_object({}) do |data, hash|
-        key = JSON.generate(data.fetch(:dependencies).sort.to_h)
+      versions = Gems
+        .versions(gemmy.name)
+        .select { _1.fetch("platform") == "ruby" }
+        .map { _1.fetch "number" }
+
+      deps = versions.each_with_object({}) do |version, hash|
+        cache_key = [
+          :deps,
+          gemmy.name,
+          version
+        ].join(":")
+        key = Rails.cache.fetch(cache_key) do
+          Gems::V2
+            .info(gemmy.name, version)
+            .deep_fetch("dependencies", "runtime")
+            .map(&:values)
+            .sort_by(&:first)
+            .to_h
+            .then { JSON.generate _1 }
+        end
         hash[key] ||= []
-        hash[key] << data.fetch(:number)
+        hash[key] << version
       end
-      gemmy.update! dependencies_and_versions: dependencies_and_versions
+
+      gemmy.update! dependencies_and_versions: deps
     end
   end
 end

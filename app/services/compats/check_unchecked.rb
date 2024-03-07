d35bf8480144e3708252ce2b029ceb3f77e97565
Manuel Meurer <manuel@meurer.io>
2024-03-07 15:50:59 +0100

    check compats of latest major rails releases first

--- app/services/compats/check_unchecked.rb
+++ app/services/compats/check_unchecked.rb
@@ -1,10 +1,30 @@
 module Compats
   class CheckUnchecked < Baseline::Service
+    LIMIT = 100
+
     def call
       check_uniqueness
 
-      Compat.unchecked.limit(100).find_each do |compat|
-        Compats::Check.call compat
+      count = 0
+
+      RailsRelease
+        .latest_major
+        .reverse
+        .concat(RailsRelease.all)
+        .uniq
+        .each do |rails_release|
+
+        break unless count < LIMIT
+
+        rails_release
+          .compats
+          .unchecked
+          .limit(LIMIT - count)
+          .each do |compat|
+
+          count += 1
+          Compats::Check.call compat
+        end
       end
     end
   end

79783e49cec9a27cab0ad2991acc8f92e2cb01e3
Manuel Meurer <manuel@meurer.io>
2024-03-10 20:45:00 +0100

    make compat checking more solid

--- app/services/compats/check_unchecked.rb
+++ app/services/compats/check_unchecked.rb
@@ -22,15 +22,39 @@ module Compats
           .limit(LIMIT - count)
           .each do |compat|
 
-          count += 1
+          next if check_failed?(compat)
 
           begin
             Compats::Check.call compat
           rescue Compats::Check::Error => error
             ReportError.call error
+            check_failed!(compat)
+          else
+            count += 1
           end
         end
       end
     end
+
+    private
+
+      def check_failed_cache_key(compat)
+        [
+          :compat_check_failed,
+          compat.id
+        ].join(":")
+      end
+
+      def check_failed?(compat)
+        Kredis.redis.exists? \
+          check_failed_cache_key(compat)
+      end
+
+      def check_failed!(compat)
+        Kredis.redis.setex \
+          check_failed_cache_key(compat),
+          1.hour,
+          nil
+      end
   end
 end

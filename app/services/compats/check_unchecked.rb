fd1a9a8b3bfe653667463e14884b4b1e4975bc41
Manuel Meurer <manuel@meurer.io>
2024-04-21 12:35:51 +0200

    fix checking unchecked compats

--- app/services/compats/check_unchecked.rb
+++ app/services/compats/check_unchecked.rb
@@ -4,51 +4,53 @@ module Compats
 
     def call
       check_uniqueness on_error: :return
-
-      count = 0
-
-      RailsRelease
-        .latest_major
-        .reverse
-        .concat(RailsRelease.all)
-        .uniq
-        .each do |rails_release|
-
-        break unless count < LIMIT
-
-        rails_release
-          .compats
-          .unchecked
-          .limit(LIMIT - count)
-          .each do |compat|
-
-          next if check_failed?(compat)
-
-          if Date.current > Date.new(2024, 6, 1)
-            ReportError.call "remove this!"
-          end
-          if compat.invalid?
-            compats = Compat.where(dependencies: compat.dependencies)
-            if compats.size == RailsRelease.count && !compats.include?(compat)
-              compat.destroy
-              next
-            end
-          end
-
-          begin
-            Compats::Check.call compat
-          rescue Compats::Check::Error => error
-            ReportError.call error,
-              compat_id: compat.id
-            check_failed!(compat)
-          else
-            count += 1
-          end
-        end
-      end
+      do_check
     end
 
     private
+
+      def do_check
+        count = 0
+
+        RailsRelease
+          .latest_major
+          .reverse
+          .concat(RailsRelease.all)
+          .uniq
+          .each do |rails_release|
+
+          rails_release
+            .compats
+            .unchecked
+            .find_each do |compat|
+
+            return unless count < LIMIT
+
+            next if check_failed?(compat)
+
+            if Date.current > Date.new(2024, 6, 1)
+              ReportError.call "remove this!"
+            end
+            if compat.invalid?
+              compats = Compat.where(dependencies: compat.dependencies)
+              if compats.size == RailsRelease.count && !compats.include?(compat)
+                compat.destroy
+                next
+              end
+            end
+
+            begin
+              Compats::Check.call compat
+            rescue Compats::Check::Error => error
+              ReportError.call error,
+                compat_id: compat.id
+              check_failed!(compat)
+            else
+              count += 1
+            end
+          end
+        end
+      end
 
       def check_failed_cache_key(compat)
         [

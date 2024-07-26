abaa9683127bb60ee4b6dacc9700d875ecbcd8a8
Ernesto Tagwerker <ernesto+github@ombulabs.com>
2024-07-26 16:07:28 -0400

    Change way to fetch compats because we are now on PG

--- app/models/concerns/has_compats.rb
+++ app/models/concerns/has_compats.rb
@@ -1,6 +1,9 @@
 module HasCompats
+  # def compats
+  #   gemmies = is_a?(Gemmy) ? self.class.where(id: self) : self.gemmies
+  #   Compat.where(id: gemmies.from("#{Gemmy.table_name}, json_each(#{Gemmy.table_name}.compat_ids)").select("json_each.value"))
+  # end
   def compats
-    gemmies = is_a?(Gemmy) ? self.class.where(id: self) : self.gemmies
-    Compat.where(id: gemmies.from("#{Gemmy.table_name}, json_each(#{Gemmy.table_name}.compat_ids)").select("json_each.value"))
+    Compat.where("id IN (SELECT value::bigint FROM json_array_elements_text(?))", compat_ids.to_json)
   end
 end

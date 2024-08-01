dd3b7a14a6f2f33c6a555bd60afd9de8a8dba0b3
Ernesto Tagwerker <ernesto+github@ombulabs.com>
2024-08-01 13:24:31 -0400

    Enable assets and change the way we release CSS in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,6 +23,7 @@ Rails.application.configure do
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
+  config.assets.enabled = true
   config.assets.compile = false
   config.assets.digest = true
   config.assets.version = '1.0'

5a502600397aa0c348b0eeb9a84eb301e14d1024
Ernesto Tagwerker <ernesto+github@ombulabs.com>
2024-08-22 23:20:07 -0400

    Set Baseline.redis

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,7 +1,10 @@
 Sidekiq.configure_server do |config|
-  config.redis = { ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE } }
+  config.redis = { ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }, url: ENV["REDIS_URL"] }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE } }
+  config.redis = { ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }, url: ENV["REDIS_URL"] }
 end
+
+$redis = Redis.new(url: ENV["REDIS_URL"], ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE })
+Baseline.redis = $redis

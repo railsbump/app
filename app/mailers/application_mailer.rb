b16b90e5a44ab54b83a3f21b27e3ab4f49b6b3b4
Ernesto Tagwerker <ernesto+github@ombulabs.com>
2024-08-22 22:17:13 -0400

    Set up SMTP in production
    This configuration will work with Amazon SES

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,6 @@
 class ApplicationMailer < ActionMailer::Base
   default \
-    from:           "RailsBump <hello@railsbump.org>",
+    from: "RailsBump <#{ENV["SMTP_FROM"]}>"
     template_path:  "mailers",
     message_stream: :outbound
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,9 +87,19 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.smtp_settings = {
+    user_name: ENV["SMTP_USERNAME"],
+    password: ENV["SMTP_PASSWORD"],
+    domain: ENV["SMTP_DOMAIN"],
+    address: ENV["SMTP_ADDRESS"],
+    port: ENV["SMTP_PORT"],
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).

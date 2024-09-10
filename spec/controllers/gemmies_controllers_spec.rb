a503ac305985ae89e57de588025d29c39b7218b6
Ernesto Tagwerker <ernesto+github@ombulabs.com>
2024-09-10 19:30:03 -0400

    Add controller spec to check post :create

---
+++ spec/controllers/gemmies_controllers_spec.rb
@@ -0,0 +1,33 @@
+require "rails_helper"
+
+RSpec.describe GemmiesController, type: :controller do
+  describe "create" do
+    context "when the gemmy params are valid" do
+      it "redirects to the new gemmy page" do
+        post :create, params: { gemmy: { name: "next_rails" } }
+  
+        expect(response).to redirect_to(gemmy_path(Gemmy.find_by(name: "next_rails")))
+      end
+
+      it "creates a record in the database" do
+        expect do
+          post :create, params: { gemmy: { name: "next_rails" } }
+        end.to change(Gemmy, :count).by(1)
+      end
+
+      context "when the gemmy params are invalid" do
+        it "renders the new gemmy page" do
+          post :create, params: { gemmy: { name: "" } }
+  
+          expect(response).to render_template(:new)
+        end
+
+        it "does not create a record in the database" do
+          expect do
+            post :create, params: { gemmy: { name: "" } }
+          end.not_to change(Gemmy, :count)
+        end
+      end
+    end
+  end
+end

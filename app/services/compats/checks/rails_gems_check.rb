4d9d5cf788d7c68f80d8cc8b9eb4abad5333c4f7
Ernesto Tagwerker <ernesto+github@ombulabs.com>
2024-10-01 21:59:58 -0400

    Move constant to RailsGemsCheck

--- app/services/compats/checks/rails_gems_check.rb
+++ app/services/compats/checks/rails_gems_check.rb
@@ -5,6 +5,22 @@ module Compats::Checks
   #
   # If that's the case, the compat is marked as incompatible.
   class RailsGemsCheck < Base
+    RAILS_GEMS = %w(
+      actioncable
+      actionmailbox
+      actionmailer
+      actionpack
+      actiontext
+      actionview
+      activejob
+      activemodel
+      activerecord
+      activestorage
+      activesupport
+      rails
+      railties
+    )
+
     def call
       return unless @compat.pending?
 

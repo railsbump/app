1a8d8555ef87f197f6df5d16fbaa1f595036a4fc
Ernesto Tagwerker <ernesto+github@ombulabs.com>
2024-10-12 16:31:19 -0400

    Refactor lockfile creation to use more of the model

--- app/models/lockfile.rb
+++ app/models/lockfile.rb
@@ -5,13 +5,61 @@ class Lockfile < ApplicationRecord
   validates :content, presence: true
   validates :slug,    presence: { if: -> { content.present? } }, uniqueness: { allow_blank: true }
   validates :gemmies, presence: { if: -> { content.present? } }
+  validate :validate_content
+  validate :validate_gemmies
+
+  before_validation :generate_slug
+  before_validation :add_gemmies
 
   delegate :to_param, to: :slug
 
   scope :with_gemmies, ->(gemmies) { joins(:gemmies).where(gemmies: { id: gemmies }).distinct }
 
+  CONTENT_REGEX = %r(
+    GEM
+    .+
+    DEPENDENCIES
+  )xm.freeze
+
   def compats
     Compat.where(id: gemmies.flat_map(&:compat_ids))
+  end
+
+  def gem_names
+    parser = Bundler::LockfileParser.new(content)
+    parser.dependencies.keys - %w(rails)
+  end
+
+  private
+
+  def add_gemmies
+    return if content.blank?
+    return if gemmies.any?
+
+    gem_names.each do |gem_name|
+      gemmy = Gemmy.find_by(name: gem_name) || Gemmies::Create.call(gem_name)
+      self.gemmies << gemmy
+    end
+  end
+
+  def generate_slug
+    return if self.slug.present?
+
+    self.slug = ActiveSupport::Digest.hexdigest(gem_names.join("#"))
+  end
+
+  def validate_content
+    unless CONTENT_REGEX.match?(content)
+      self.errors.add(:content, "does not look like a valid lockfile.")
+    end
+  end
+
+  def validate_gemmies
+    parser = Bundler::LockfileParser.new(content)
+
+    if gem_names.none?
+      self.errors.add(:content, "No gems found in content.")
+    end
   end
 end
 
--- app/services/lockfiles/create.rb
+++ app/services/lockfiles/create.rb
@@ -2,12 +2,6 @@ require "bundler/lockfile_parser"
 
 module Lockfiles
   class Create < Baseline::Service
-    CONTENT_REGEX = %r(
-      GEM
-      .+
-      DEPENDENCIES
-    )xm
-
     class AlreadyExists < Error
       attr_reader :lockfile
 
@@ -19,35 +13,23 @@ module Lockfiles
     end
 
     def call(content)
-      lockfile = Lockfile.new(content: content)
+      lockfile = build(content)
 
-      if content.present?
-        unless CONTENT_REGEX.match?(content)
-          raise Error, "This does not look like a valid lockfile."
-        end
+      lockfile.tap(&:save)
+    end
 
-        parser    = Bundler::LockfileParser.new(content)
-        gem_names = parser.dependencies.keys - %w(rails)
+    def build(content)
+      result = Lockfile.new(content: content)
 
-        if gem_names.none?
-          raise Error, "No gems found in content."
-        end
-
-        lockfile.slug = ActiveSupport::Digest.hexdigest(gem_names.join("#"))
-
-        if existing_lockfile = Lockfile.find_by(slug: lockfile.slug)
+      if result.valid?
+        if existing_lockfile = Lockfile.find_by(slug: result.slug)
           raise AlreadyExists.new(existing_lockfile)
         end
 
-        gem_names.each do |gem_name|
-          gemmy = Gemmy.find_by(name: gem_name) || Gemmies::Create.call(gem_name)
-          lockfile.gemmies << gemmy
-        rescue Gemmies::Create::NotFound
-          next
-        end
+        result
+      else
+        result
       end
-
-      lockfile.tap(&:save!)
     end
   end
 end

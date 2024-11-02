80b4ade596eef22f3b0cc75a584d7bcda76e4faa
Ernesto Tagwerker <ernesto+github@ombulabs.com>
2024-11-01 23:20:57 -0400

    Correct sitemap.xml path

--- app/controllers/sitemaps_controller.rb
+++ app/controllers/sitemaps_controller.rb
@@ -5,7 +5,7 @@ class SitemapsController < ApplicationController
   def show
     sitemap_url = ENV["FOG_URL"]
     if sitemap_url.present?
-      send_data open(sitemap_url).read, type: "application/xml", disposition: "inline"
+      send_data open("#{sitemap_url}/sitemap.xml").read, type: "application/xml", disposition: "inline"
     else
       head :not_found
     end

f7bb929e63b5fb4b8b9d1e17c613c4e9558d2994
Ernesto Tagwerker <ernesto+github@ombulabs.com>
2024-12-09 08:35:48 -0500

    Add meta description support
    This will improve visibility in search results

--- app/helpers/rails_releases_helper.rb
+++ app/helpers/rails_releases_helper.rb
@@ -13,4 +13,19 @@ module RailsReleasesHelper
       super
     end
   end
+
+  def meta_description
+    if @gemmy
+      if @rails_release
+        "RailsBump calculated the compatibility status for the #{@gemmy} gem and #{@rails_release}. Is #{@gemmy} compatible with #{@rails_release}?"
+      else
+        rails_releases = RailsRelease.order(:version)
+        first_version = rails_releases.first.version
+        last_version = rails_releases.last.version
+        "RailsBump calculated #{@gemmy}'s compatibility with Rails #{first_version} to #{last_version}. How compatible the #{@gemmy} gem is with different versions of Rails?"
+      end
+    else
+      super
+    end
+  end
 end

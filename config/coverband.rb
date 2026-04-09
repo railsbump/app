7cae2673a1e00e3d942be88a53e976ef458927fe
Juan Vásquez <juan@ombulabs.com>
2026-04-09 12:32:11 -0600

    Restore explicit config.track_views = true

--- config/coverband.rb
+++ config/coverband.rb
@@ -3,6 +3,7 @@ Coverband.configure do |config|
     Redis.new(url: ENV["REDIS_URL"], ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE })
   )
   config.logger = Rails.logger
+  config.track_views = true
   config.background_reporting_enabled = true
   config.web_enable_clear = true
   config.ignore = %w[config/boot.rb config/environment.rb config/puma.rb bin/]

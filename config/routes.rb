c1101b3ea36f98dc5953cc506fbf4c5fb19ba421
Juan Vásquez <javasgon@gmail.com>
2026-04-23 15:38:54 -0600

    Remove coverband

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,6 @@ end
 
 Rails.application.routes.draw do
   mount Sidekiq::Web => "sidekiq"
-  mount Coverband::Reporters::Web.new, at: "/coverband" if ENV["COVERBAND_PASSWORD"].present?
 
   get '/sitemap.xml', to: 'sitemaps#show'
   get "/robots.txt" => "static#robots"

0757c28520ccafef2bc4376975fe9fdd75114987
Juan Vásquez <juan@ombulabs.com>
2026-06-02 11:23:37 -0600

    Address Code Review
    Update the rerender to granular per gem_check update in the UI
    
     Co-authored-by: Ariel <ariel@ombulabs.com>

--- app/services/checks/resolve_gem.rb
+++ app/services/checks/resolve_gem.rb
@@ -11,16 +11,34 @@ module Checks
       gem_check.perform!
 
       lockfile_check = gem_check.lockfile_check
+      broadcast_gem_check(gem_check)
       mark_lockfile_check_complete(lockfile_check)
-      lockfile_check.lockfile.broadcast_checks
     end
 
     private
+
+    def broadcast_gem_check(gem_check)
+      lockfile = gem_check.lockfile_check.lockfile
+
+      Turbo::StreamsChannel.broadcast_replace_to(
+        lockfile, :gem_checks,
+        target: ActionView::RecordIdentifier.dom_id(gem_check),
+        partial: "gem_checks/gem_check",
+        locals: { gem_check: gem_check }
+      )
+    end
 
     def mark_lockfile_check_complete(lockfile_check)
       return if lockfile_check.gem_checks.where(status: "pending").exists?
 
       lockfile_check.update!(status: "complete")
+
+      Turbo::StreamsChannel.broadcast_replace_to(
+        lockfile_check.lockfile, :gem_checks,
+        target: ActionView::RecordIdentifier.dom_id(lockfile_check, :status),
+        partial: "lockfile_checks/status_badge",
+        locals: { lockfile_check: lockfile_check }
+      )
     end
   end
 end
